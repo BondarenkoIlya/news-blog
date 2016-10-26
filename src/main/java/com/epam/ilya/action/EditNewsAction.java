7ae081b4bfd668f66188dc44ee8030be78128da7
admin <mr_spec@mail.ru>
2016-10-26 15:10:24 +0600

    Add empty actions

---
+++ src/main/java/com/epam/ilya/action/EditNewsAction.java
@@ -0,0 +1,6 @@
+package com.epam.ilya.action;
+
+import org.apache.struts.action.Action;
+
+public class EditNewsAction extends Action {
+}
---
+++ src/main/java/com/epam/ilya/action/ShowNewsListPageAction.java
@@ -0,0 +1,6 @@
+package com.epam.ilya.action;
+
+import org.apache.struts.action.Action;
+
+public class ShowNewsListPageAction extends Action {
+}
