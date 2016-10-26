2f5170d6e96e6625230b47c660ad9031132f1a9d
admin <mr_spec@mail.ru>
2016-10-26 18:03:06 +0600

    Test for branch

---
+++ src/main/java/com/epam/ilya/form/NewsForm.java
@@ -0,0 +1,6 @@
+package com.epam.ilya.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class NewsForm extends ActionForm {
+}
---
+++ src/main/java/com/epam/ilya/form/NewsListForm.java
@@ -0,0 +1,6 @@
+package com.epam.ilya.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class NewsListForm extends ActionForm {
+}
