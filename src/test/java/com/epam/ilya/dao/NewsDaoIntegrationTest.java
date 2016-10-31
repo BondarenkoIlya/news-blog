8df6eb57384d137cc924065654c08fae05e9fe4b
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-10-31 17:45:21 +0600

    Create some tests

---
+++ src/test/java/com/epam/ilya/dao/NewsDaoIntegrationTest.java
@@ -0,0 +1,61 @@
+package com.epam.ilya.dao;
+
+import com.epam.ilya.model.BaseEntity;
+import com.epam.ilya.model.News;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Real implementation called.
+ */
+public class NewsDaoIntegrationTest {
+
+    @Test
+    public void create() throws Exception {
+        News news = getTestNews();
+        NewsDao newsDao = new NewsDao();
+        newsDao.create(news);
+        assertTrue(news.getId() > 0);
+    }
+
+    @Test
+    public void findById() throws Exception {
+        News news = getTestNews();
+        news.setContent("New test");
+        NewsDao newsDao = new NewsDao();
+        news = newsDao.create(news);
+        News foundNews = newsDao.findById(news.getId());
+        assertTrue(news.getTitle().equals(foundNews.getTitle()));
+    }
+
+    @Test
+    public void update() throws Exception {
+        News news = getTestNews();
+        NewsDao newsDao = new NewsDao();
+        news = newsDao.create(news);
+        news.setTitle("New test");
+        newsDao.update(news);
+        News foundNews = newsDao.findById(news.getId());
+        assertTrue(news.getTitle().equals(foundNews.getTitle()));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        News news = getTestNews();
+        NewsDao newsDao = new NewsDao();
+        newsDao.delete(news);
+        News foundNews = newsDao.findById(news.getId());
+        assertTrue(foundNews.getTitle() == null);
+    }
+
+    private News getTestNews() {
+        News news = new News();
+        news.setTitle("test");
+        news.setBrief("test");
+        news.setContent("test");
+        news.setStatus(BaseEntity.ACTIVE);
+        return news;
+    }
+
+}
---
+++ src/test/java/com/epam/ilya/model/NewsTest.java
@@ -0,0 +1,27 @@
+package com.epam.ilya.model;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class NewsTest {
+
+    private static final String BREAKING_NEWS_TRUMP_WINS = "Breaking news: Trump wins";
+
+    @Test
+    public void shouldReturnSpecifiedTitleWhenSetIt() throws Exception {
+        // given
+        News item = new News();
+        // when
+        item.setTitle(BREAKING_NEWS_TRUMP_WINS);
+        // then
+        assertThat(item.getTitle(), is(BREAKING_NEWS_TRUMP_WINS)); // hamcrest
+    }
+
+    @Test
+    public void shouldReturnAllInformationAboutNewsWhenToStringIsCalled() throws Exception {
+
+
+    }
+}
