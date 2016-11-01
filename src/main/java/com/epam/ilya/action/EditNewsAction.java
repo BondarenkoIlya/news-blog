6ed7d803dcb35d53dbecf1c702d6a39544bc8cc7
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-11-01 17:24:58 +0600

    #39 Write actions with news

--- src/main/java/com/epam/ilya/action/EditNewsAction.java
+++ src/main/java/com/epam/ilya/action/EditNewsAction.java
@@ -1,6 +1,28 @@
 package com.epam.ilya.action;
 
-import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
 
-public class EditNewsAction extends Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditNewsAction extends DispatchAction {
+
+
+    public ActionForward update(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return mapping.findForward("success");
+    }
+
+    public ActionForward delete(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return mapping.findForward("success");
+    }
+
+    public ActionForward add(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return mapping.findForward("success");
+    }
 }
--- src/main/java/com/epam/ilya/action/ShowNewsEditionPageAction.java
+++ src/main/java/com/epam/ilya/action/ShowNewsEditionPageAction.java
@@ -1,6 +1,33 @@
 package com.epam.ilya.action;
 
+import com.epam.ilya.form.NewsForm;
+import com.epam.ilya.model.News;
+import com.epam.ilya.service.NewsService;
+import com.epam.ilya.service.ServiceException;
 import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ShowNewsEditionPageAction extends Action {
+
+    private static final String ID = "id";
+    
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String id = request.getParameter(ID);
+        NewsService service = new NewsService();
+        News news;
+        try {
+            news = service.getNewsById(id);
+        } catch (ServiceException e) {
+            throw new ActionException("Cannot get news by id",e);
+        }
+        NewsForm newsForm = (NewsForm) form;
+        newsForm.setNews(news);
+        return mapping.findForward("success");
+    }
 }
