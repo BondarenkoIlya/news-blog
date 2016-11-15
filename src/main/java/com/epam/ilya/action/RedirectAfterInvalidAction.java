063ee448fad8872801d62a9c9ac6c97d520c70ef
Home <bondarenko_elijah@mail.ru>
2016-11-16 00:38:43 +0600

    #91 Fix bug with unexecutable method

--- src/main/java/com/epam/ilya/action/RedirectAfterInvalidAction.java
+++ src/main/java/com/epam/ilya/action/RedirectAfterInvalidAction.java
@@ -5,18 +5,13 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class RedirectAfterInvalidAction extends Action {
 
     @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form, ServletRequest request, ServletResponse response) throws Exception {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String id = (String) req.getSession().getAttribute("id");
-        String ref = (String) req.getSession().getAttribute("ref");
-        ref = "";
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         return mapping.findForward("newsEdition");
     }
 }
