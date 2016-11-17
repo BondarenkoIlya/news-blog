e8bd1e6af0f1269cb07f3320fdaa9b52290d11b7
BondarenkoIlya <bondarenko_elijah@mail.ru>
2016-11-17 13:58:40 +0600

    #92 Add validation xml

--- src/main/java/com/epam/ilya/form/CommentForm.java
+++ src/main/java/com/epam/ilya/form/CommentForm.java
@@ -1,12 +1,7 @@
 package com.epam.ilya.form;
 
 import com.epam.ilya.model.Comment;
-import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
 import org.apache.struts.validator.ValidatorForm;
-
-import javax.servlet.http.HttpServletRequest;
 
 public class CommentForm extends ValidatorForm {
     private Comment newComment = new Comment();
